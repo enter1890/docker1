2bdfa42eef994ee911eedca2d769160cf0ace5a7
enter1890 <93629478+enter1890@users.noreply.github.com>
2024-07-21 08:15:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const GUGU_AUTH = process.env.GUGU_AUTH || '';
 const CFIP = process.env.CFIP || 'skk.moe';
 const CFPORT = process.env.CFPORT || 443;
 const NAME = process.env.NAME || 'Vls';
-const ARGO_PORT = process.env.ARGO_PORT || 28800;
-const PORT = process.env.SERVER_PORT || process.env.PORT || 27000;
+const ARGO_PORT = process.env.ARGO_PORT || 34562;
+const PORT = process.env.SERVER_PORT || process.env.PORT || 36900;
 
 
 if (!fs.existsSync(FILE_PATH)) {
